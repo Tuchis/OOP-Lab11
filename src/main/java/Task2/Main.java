4ddddc8ddc0dc75d435aa0de5e36979e7d98f8d3
Vladyslav Humennyy <54081501+Tuchis@users.noreply.github.com>
2023-01-04 16:33:22 +0200

    Task2 Main

---
+++ src/main/java/Task2/Main.java
@@ -0,0 +1,36 @@
+package Task2;
+
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.resource.Emailv31;
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class Main {
+    public static void main(String[] args) throws MailjetException, MailjetSocketTimeoutException {
+        MailjetClient client;
+        MailjetRequest request;
+        MailjetResponse response;
+        client = new MailjetClient(System.getenv("c59cee5cebe03246fd871f521b067fd0"), System.getenv("53918edeabdaf1cf2cf2a4a6a9319e9a"), new ClientOptions("v3.1"));
+        request = new MailjetRequest(Emailv31.resource)
+                .property(Emailv31.MESSAGES, new JSONArray()
+                        .put(new JSONObject()
+                                .put(Emailv31.Message.FROM, new JSONObject()
+                                        .put("Email", "vladyslav.humennyi@ucu.edu.ua")
+                                        .put("Name", "Vladyslav"))
+                                .put(Emailv31.Message.TO, new JSONArray()
+                                        .put(new JSONObject()
+                                                .put("Email", "vladyslav.humennyi@ucu.edu.ua")
+                                                .put("Name", "Vladyslav")))
+                                .put(Emailv31.Message.SUBJECT, "Greetings from Mailjet.")
+                                .put(Emailv31.Message.TEXTPART, "My first Mailjet email")
+                                .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!")
+                                .put(Emailv31.Message.CUSTOMID, "AppGettingStartedTest")));
+        response = client.post(request);
+        System.out.println(response.getStatus());
+        System.out.println(response.getData());
+    }
+}
